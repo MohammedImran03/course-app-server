b084689f2623e03956785f2e0092f2525f181e8b
MohammedImran03 <mohammedimran1407.mi@gmail.com>
2024-04-03 19:16:05 +0530

    first commit

--- Router/PaymentRouter.js
+++ Router/PaymentRouter.js
@@ -5,16 +5,33 @@ const stripeInstance = stripe('sk_test_51OuEqlSBQwTu3SdvMiUxrJ1x5eC5nDESdMA7SAwe
 
 router.post('/intents', async (req, res) => {
     try {
-        console.log('paymentmethod');
+        const {name,email}=req.body;
+        console.log('paymentmethod'); 
+        const customer=await stripeInstance.customers.create({
+            name,
+            email,
+        });
+        // const ephemeralKey=await stripeInstance.ephemeralKeys.create({
+        //   issuing_card: 'ic_1ITi6XKYfU8ZP6raDAXem8ql',
+        // },
+        //     {apiVersion : '2023-10-16',});
         const total=parseInt(req.body.amount * 100);
         const paymentIntent = await stripeInstance.paymentIntents.create({
             amount: total, 
             currency: 'inr',
-            payment_method_types: ['card'],
+            customer:customer.id,
+            automatic_payment_methods:{
+                enabled : true,
+            },
         });
         console.log(paymentIntent);
-        res.json({ paymentIntent: paymentIntent.client_secret });
+        res.json({ paymentIntent: paymentIntent.client_secret,
+            // ephemeralKey_Secret:ephemeralKey.secret,
+            customer:customer.id,
+            // publishableKey:'sk_test_51OuEqlSBQwTu3SdvMiUxrJ1x5eC5nDESdMA7SAwehkFkaabJ8hEavopdROukpefBZet0pWZb5h9vPJkWKPZQ1VAU00QPr2pRwb'        
+        });
     } catch (e) {
+        console.log(e);
         res.status(400).json({
             error: e.message,
         });
