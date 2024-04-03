e628faf0a88909851f869ed1eb10d23a2cf5e170
MohammedImran03 <mohammedimran1407.mi@gmail.com>
2024-04-03 12:01:16 +0530

    debug

--- Router/PaymentRouter.js
+++ Router/PaymentRouter.js
@@ -1,23 +1,24 @@
 import express from "express";
 const router = express.Router();
-import stripe from 'stripe';
+import stripe from 'stripe'; // Corrected import statement
 const stripeInstance = stripe('sk_test_51OuEqlSBQwTu3SdvMiUxrJ1x5eC5nDESdMA7SAwehkFkaabJ8hEavopdROukpefBZet0pWZb5h9vPJkWKPZQ1VAU00QPr2pRwb');
 
 router.post('/intents', async (req, res) => {
     try {
-console.log('paymentmethod');
-      const paymentIntent = await stripeInstance.paymentIntents.create({
-        amount: req.body.amount *100, 
-        currency: 'inr',
-        payment_method_types: [ 'card' ],
-      });
-      console.log(paymentIntent);
-      res.json({ paymentIntent: paymentIntent.client_secret });
+        console.log('paymentmethod');
+        const total=parseInt(req.body.amount * 100);
+        const paymentIntent = await stripeInstance.paymentIntents.create({
+            amount: total, 
+            currency: 'inr',
+            payment_method_types: ['card'],
+        });
+        console.log(paymentIntent);
+        res.json({ paymentIntent: paymentIntent.client_secret });
     } catch (e) {
-      res.status(400).json({
-        error: e.message,
-      });
+        res.status(400).json({
+            error: e.message,
+        });
     }
-  });
+});
 
 export const Paymentrouter = router;
